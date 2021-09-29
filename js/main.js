a4cc99fa1d1dfffa27c630163c3e8285c4be1d0c
Mark33Mark <mark@watsonised.com>
2021-09-29 22:44:57 +1000

    Updated Javascript that is now Fetching and compiling recipes. Needs a lot of work with the aesthetics and mixing up CSS a bit.

--- js/main.js
+++ js/main.js
@@ -9,16 +9,14 @@
  *                                                                                             *
  * =========================================================================================== */
 
-let spoonacularAPI  = "3f02a89ca80e407492794df034986041";
+const spoonacularAPI  = "3f02a89ca80e407492794df034986041";
 
-let userIngredient   = document.querySelector("#recipeIngredient");
-let recipesFound    = document.querySelector("#theRecipes");
-let searchForm      = document.querySelector("#search-form");
-
-let recipeList    = document.createElement( "DIV" );
-let table         = document.createElement( "TABLE" );
-let tableBody     = document.createElement( "TBODY" );
-
+const userIngredient  = document.querySelector("#recipeIngredient");
+const recipesFound    = document.querySelector("#theRecipes");
+const searchForm      = document.querySelector("#search-form");
+const mealList        = document.getElementById("meal");
+const recipeContent   = document.querySelector(".recipe-content");
+const recipeCloseBtn  = document.getElementById("recipe-close-btn");
 
 let ingredient      = [];
 let userPref        = [];
@@ -50,18 +48,19 @@ function getTodaysRecipe ( ingredientProvided ) {
 
     .then(function ( response ) {
       if ( response.ok ) {
-      response.json()
-      
+        response.json()
 
         .then(function ( data ) {
 
             console.log( data );
             listMe( data );
 
-
           }); console.log(ingredientProvided);
           } else {
-            alert("I can't find that ingredient, check your spelling or possibly I just don't have data for that ingredient.");
+            let html = "";
+            html = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+            mealList.innerHTML = html;
           } 
     });
 };
@@ -70,31 +69,78 @@ function getTodaysRecipe ( ingredientProvided ) {
 
 function listMe( recipes ) {
 
+  let html = "";
 
+  recipes.results.forEach( function (result){
 
+    console.log(result);
 
-  for ( let i = 0; i <= recipes.results.length-1; i++ ){
+    html += `
+        <div class = "w3-card-4 w3-margin w3-center" style="flex: 0 1 30%; width:50%;" data-id = "${result.id}">
+            <div class = "meal-img">
+                <img src = "${result.image}" alt = "food">
+            </div>
+            <div class = "meal-name">
+                <h3>${result.title}</h3>
+                <a href = "#" class = "recipe-btn">Get Recipe</a>
+            </div>
+        </div>
+    `;
+    mealList.innerHTML = html;
+});
+mealList.classList.remove('notFound');
 
-    console.log(recipes.results[i].title);
+}
 
-    let recipeItem  = document.createElement("div");
+/* =========================================================================================== */
 
-    recipeItem.classList    = "w3-btn w3-large w3-dark-grey w3-hover-light-grey w3-col w3-s10";
-    recipeItem.setAttribute( "id", "recipe" + i );
-    recipeItem.textContent = recipes.results[i].title;
-    recipesFound.prepend(recipeItem); 
+function getMealRecipe( event ){
 
+  event.preventDefault();
+  
+  if( event.target.classList.contains("recipe-btn")){
 
+      let recipeItem = event.target.parentElement.parentElement;
 
+      console.log(recipeItem.dataset.id);
+      
+      fetch(`https://api.spoonacular.com/recipes/${recipeItem.dataset.id}/information?query=includeNutrition=true&apiKey=${spoonacularAPI}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal( data ));
   }
 }
 
 
+function mealRecipeModal( meal ){
+  console.log(meal);
 
-/* =========================================================================================== */
+  let html = `
+      <h2 class = "recipe-title">${meal.title}</h2>
+      <div class = "recipe-summary">
+          <h3>Recipe idea:</h3>
+          <p>${meal.summary}</p>
+      </div>
+      <div class = "recipe-instruct">
+          <h3>Instructions:</h3>
+          <p>${meal.instructions}</p>
+      </div>
+      <div class = "recipe-meal-img">
+          <img src = "${meal.image}" alt = "">
+      </div> `;
+      recipeContent.innerHTML = html;
+      recipeContent.parentElement.classList.add('showRecipe');
+}
 
+
+/* == Events ==================================================================================== */
 
 searchForm.addEventListener('submit', handleIngredientEntered);
+
+mealList.addEventListener('click', getMealRecipe);
+
+recipeCloseBtn.addEventListener('click', () => {
+  recipeContent.parentElement.classList.remove('showRecipe');
+});
 
 
 /* ============================================================================================ 
